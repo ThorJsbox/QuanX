d147d7b6d1e5fff08b82375ccea496b24f7bb0d3
ThorJsbox <60847636+ThorJsbox@users.noreply.github.com>
2021-06-19 12:19:56 +0800

    Update jd_fruit.js

--- JDJS/long-term/jd_fruit.js
+++ JDJS/long-term/jd_fruit.js
@@ -12,7 +12,7 @@ let shareCodes = [  // 这个列表填入你要助力的好友的shareCode
   //'20bab5d0edc044a999216db8872cec04@e8b5580c693042b3b276b693c4e30b8b',
 ]
 let message = '', subTitle = '', option = {}, isFruitFinished = false;
-const retainWater = 100;//保留水滴大于多少g,默认100g;
+const retainWater = 50;//保留水滴大于多少g,默认100g;
 let jdNotify = false;//是否关闭通知，false打开通知推送，true关闭通知推送
 let jdFruitBeanCard = false;//农场使用水滴换豆卡(如果出现限时活动时100g水换20豆,此时比浇水划算,推荐换豆),true表示换豆(不浇水),false表示不换豆(继续浇水),脚本默认是浇水
 let randomCount = $.isNode() ? 20 : 5;
