891205ef2c998b62b23b793fc830b12dc4ed01dd
ThorJsbox <60847636+ThorJsbox@users.noreply.github.com>
2021-12-27 16:04:53 +0800

    Update price.js

--- Rewrite_local/price.js
+++ Rewrite_local/price.js
@@ -1,30 +1,40 @@
 /*
-jd tb 商品历史价格查询，比价
+JD | TB Price comparison
+by Small
+date 2021-12-07
+Thanks @yichahucha
+修改32行Surge语法，兼容淘宝比价
 
-QuantumultX:
-^https?://api\.m\.jd\.com/client\.action\?functionId=(wareBusiness|serverConfig|basicConfig) url script-response-body https://raw.githubusercontent.com/zwf234/rules/master/js/price.js
+QX:
+^https?://api\.m\.jd\.com/client\.action\?functionId=(wareBusiness|serverConfig|basicConfig) url script-response-body https://service.2ti.st/QuanX/Script/jd_tb_price/main.js
 
-^http://.+/amdc/mobileDispatch url script-request-body https://raw.githubusercontent.com/zwf234/rules/master/js/price.js
-^https?://trade-acs\.m\.taobao\.com/gw/mtop\.taobao\.detail\.getdetail url script-response-body https://raw.githubusercontent.com/zwf234/rules/master/js/price.js
+^http://.+/amdc/mobileDispatch url script-request-body https://service.2ti.st/QuanX/Script/jd_tb_price/main.js
+^https?://trade-acs\.m\.taobao\.com/gw/mtop\.taobao\.detail\.getdetail url script-response-body https://service.2ti.st/QuanX/Script/jd_tb_price/main.js
 
 Surge4:
-http-response ^https?://api\.m\.jd\.com/client\.action\?functionId=(wareBusiness|serverConfig|basicConfig) requires-body=1,script-path=https://raw.githubusercontent.com/zwf234/rules/master/js/price.js
+http-response ^https?://api\.m\.jd\.com/client\.action\?functionId=(wareBusiness|serverConfig|basicConfig) requires-body=1,script-path=https://service.2ti.st/QuanX/Script/jd_tb_price/main.js
 
-http-request ^http://.+/amdc/mobileDispatch requires-body=1,script-path=https://raw.githubusercontent.com/zwf234/rules/master/js/price.js
-http-response ^https?://trade-acs\.m\.taobao\.com/gw/mtop\.taobao\.detail\.getdetail requires-body=1,script-path=https://raw.githubusercontent.com/zwf234/rules/master/js/price.js
+http-request ^http://.+/amdc/mobileDispatch requires-body=1,script-path=https://service.2ti.st/QuanX/Script/jd_tb_price/main.js
+http-response ^https?://trade-acs\.m\.taobao\.com/gw/mtop\.taobao\.detail\.getdetail requires-body=1,script-path=https://service.2ti.st/QuanX/Script/jd_tb_price/main.js
 
-MITM = api.m.jd.com, trade-acs.m.taobao.com
+Surge & QX MITM = api.m.jd.com, trade-acs.m.taobao.com
 */
 
 const ScriptName = "京东|淘宝 比价";
 const $ = new Env(ScriptName);
 
 const ScriptIdentifier = "jd_tb_price";
-const ScriptVersion = 5;
+const ScriptVersion = 8;
 const ScriptUrl = `https://service.2ti.st/QuanX/Script/${ScriptIdentifier}`
 
 const res = $request;
-const resp = isUndefined($response) ? null : $response;
+
+let resp = null;
+try{
+  resp =$response
+}catch(err){
+  console.log(err)
+};
 
 let Status = {
     Enable: 1,
@@ -314,36 +324,53 @@ function handleBijiago(data) {
         return data.msg;
 
     let obj = data.data;
+    let store = {};
 
-    let store = obj['store'][1];
+    if (obj['store'].length == 0) {
+        return "";
+    }
+
+    if (obj['store'].length == 1) {
+        store = obj['store'][0];
+    }
+    else if (obj['store'].length > 1) {
+        store = obj['store'][1];
+    }
+
+    let tips = "无tips";
+    if (obj.hasOwnProperty("analysis")) {
+        if (obj['analysis'].hasOwnProperty("tip")) {
+            tips = obj['analysis']['tip'];
+        }
+    }
 
     let historyObj = {
         tips: {
             "type": "text",
-            "title": "Tips:",
-            "text": obj['analysis']['tip'],
+            "title": "¥",
+            "text": tips,
         },
         range: {
             "type": "text",
             "title": "价格区间",
-            "text": store['price_range'],
+            "text": store.hasOwnProperty("price_range") ? store['price_range'] : "",
         },
         now: {
             "type": "price",
             "title": "当前价",
-            "price": Math.round(store['last_price'] / 100),
+            "price": Math.round(parseFloat(store['last_price']) / 100),
             "date": "-"
         },
         highest: {
             "type": "price",
             "title": "最高价",
-            "price": Math.round(store['highest']),
+            "price": Math.round(parseFloat(store['highest'])),
             "date": time2str(store['max_stamp'] * 1000)
         },
         lowest: {
             "type": "price",
             "title": "最低价",
-            "price": Math.round(store['lowest']),
+            "price": Math.round(parseFloat(store['lowest'])),
             "date": time2str(parseInt(store['min_stamp']) * 1000)
         },
         day30: {
@@ -418,6 +445,49 @@ function handleBijiago(data) {
     return result;
 }
 
+function getBijiagoCookie(callback) {
+        let cookie = get_tag("cookie");
+        if (is_tag_exp("cookie") && cookie != "") {
+            callback(cookie);
+            return;
+        }
+
+        $.log('get bijiago cookie');
+
+        const option = {
+            url: `https://browser.bijiago.com/extension?ac=bdextPermanent&format=json&version=${new Date().getTime()}`,
+            headers: {
+                'Connection': 'keep-alive',
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36',
+                'Accept': '*/*',
+                'Accept-Language': 'zh-CN,zh;q=0.9',
+            },
+            timeout: 2000
+        };
+
+        $.get(option, (err, rsp, data) => {
+            if (err) {
+                $.log(`Error:${err}`);
+                callback("");
+                return;
+            }
+
+            var cookie = "";
+
+            var cookie_raw = rsp["headers"]["Set-Cookie"];
+
+            cookie_raw.split("path=/,").forEach(item => {
+                if (item.indexOf("gwdang_permanent") != -1) {
+                    cookie += item.split(";")[0] + ";";
+                }
+            });
+
+            set_tag_exp("cookie", cookie, 1000 * 60 * 60 * 12);
+
+            callback(cookie);
+        });
+}
+
 function request_history_price(id, type, callback) {
 
     let item_url = "";
@@ -428,42 +498,46 @@ function request_history_price(id, type, callback) {
         item_url = `https://detail.tmall.com/item.htm?id=${id}`;
     }
 
-    const option = {
-        url: `https://browser.bijiago.com/extension/price_towards?url=${encodeURIComponent(item_url)}&format=jsonp&union=union_bijiago&from_device=bijiago&version=${new Date().getTime()}`,
-        headers: {
-            'Connection': 'keep-alive',
-            'sec-ch-ua': '" Not A;Brand";v="99", "Chromium";v="90", "Google Chrome";v="90"',
-            'sec-ch-ua-mobile': '?0',
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36',
-            'Accept': '*/*',
-            'Sec-Fetch-Site': 'cross-site',
-            'Sec-Fetch-Mode': 'no-cors',
-            'Sec-Fetch-Dest': 'script',
-            'Referer': item_url,
-            'Accept-Language': 'zh-CN,zh;q=0.9'
-        },
-        timeout: 2000
-    };
+    getBijiagoCookie(cookie => {
 
-    $.get(option, (err, rsp, data) => {
-        let result = {
-            success: true,
-            msg: "Success",
-            data: JSON.parse(data)
+        const option = {
+            url: `https://browser.bijiago.com/extension/price_towards?url=${encodeURIComponent(item_url)}&format=jsonp&union=union_bijiago&from_device=bijiago&version=${new Date().getTime()}`,
+            headers: {
+                'Connection': 'keep-alive',
+                'sec-ch-ua': '" Not A;Brand";v="99", "Chromium";v="90", "Google Chrome";v="90"',
+                'sec-ch-ua-mobile': '?0',
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36',
+                'Accept': '*/*',
+                'Sec-Fetch-Site': 'cross-site',
+                'Sec-Fetch-Mode': 'no-cors',
+                'Sec-Fetch-Dest': 'script',
+                'Referer': item_url,
+                'Accept-Language': 'zh-CN,zh;q=0.9',
+                'Cookie': cookie
+            },
+            timeout: 2000
         };
 
-        if (err) {
-            result.success = false;
-            result.msg = "获取价格信息失败";
-            result.data = err;
-        }
+        $.get(option, (err, rsp, data) => {
+            let result = {
+                success: true,
+                msg: "Success",
+                data: JSON.parse(data)
+            };
 
-        if (!result.success) {
-            $.log(`Error:${result}`);
-            $.msg("Error", result.msg, err);
-        }
+            if (err) {
+                result.success = false;
+                result.msg = "获取价格信息失败";
+                result.data = err;
+            }
 
-        callback(result);
+            if (!result.success) {
+                $.log(`Error:${result}`);
+                $.msg("Error", result.msg, err);
+            }
+
+            callback(result);
+        });
     });
 }
 
@@ -722,6 +796,33 @@ function handleJob(job) {
     }
 }
 
+function is_tag_exp(k) {
+    let nowTime = new Date().getTime();
+
+    let kCacheExpirationTime = `time_${ScriptIdentifier}_${k}_cacheExpirationTime`;
+    let vCacheExpirationTime = $.getdata(kCacheExpirationTime);
+
+    if (isNull(vCacheExpirationTime)) {
+        return false;
+    }
+
+    vCacheExpirationTime = parseInt(vCacheExpirationTime);
+
+    return vCacheExpirationTime > nowTime;
+}
+
+function get_tag(k) {
+    return $.getdata(k);
+}
+
+function set_tag_exp(k, v, t) {
+    $.setdata(v, k);
+
+    let kCacheExpirationTime = `time_${ScriptIdentifier}_${k}_cacheExpirationTime`;
+    let vCacheExpirationTime = new Date().getTime() + t;
+    $.setdata(String(vCacheExpirationTime), kCacheExpirationTime);
+}
+
 function checkVersion(callback = () => { }) {
     let checkVersionKey = `time_${ScriptIdentifier}_checkVersion_lastTime`;
     let nowTime = new Date().getTime();
@@ -765,6 +866,20 @@ function checkVersion(callback = () => { }) {
     }
 }
 
+function genUUID() {
+    var s = [];
+    var hexDigits = "0123456789abcdef";
+    for (var i = 0; i < 36; i++) {
+        s[i] = hexDigits.substr(Math.floor(Math.random() * 0x10), 1);
+    }
+    s[14] = "4";  // bits 12-15 of the time_hi_and_version field to 0010
+    s[19] = hexDigits.substr((s[19] & 0x3) | 0x8, 1);  // bits 6-7 of the clock_seq_hi_and_reserved to 01
+    s[8] = s[13] = s[18] = s[23] = "-";
+
+    var uuid = s.join("");
+    return uuid;
+}
+
 function isUndefined(obj) {
     return typeof obj === "undefined";
 }
@@ -774,3 +889,4 @@ function isNull(obj) {
 }
 
 function Env(t, e) { class s { constructor(t) { this.env = t } send(t, e = "GET") { t = "string" == typeof t ? { url: t } : t; let s = this.get; return "POST" === e && (s = this.post), new Promise((e, i) => { s.call(this, t, (t, s, r) => { t ? i(t) : e(s) }) }) } get(t) { return this.send.call(this.env, t) } post(t) { return this.send.call(this.env, t, "POST") } } return new class { constructor(t, e) { this.name = t, this.http = new s(this), this.data = null, this.dataFile = "box.dat", this.logs = [], this.isMute = !1, this.isNeedRewrite = !1, this.logSeparator = "\n", this.startTime = (new Date).getTime(), Object.assign(this, e), this.log("", `\ud83d\udd14${this.name}, \u5f00\u59cb!`) } isNode() { return "undefined" != typeof module && !!module.exports } isQuanX() { return "undefined" != typeof $task } isSurge() { return "undefined" != typeof $httpClient && "undefined" == typeof $loon } isLoon() { return "undefined" != typeof $loon } isShadowrocket() { return "undefined" != typeof $rocket } toObj(t, e = null) { try { return JSON.parse(t) } catch { return e } } toStr(t, e = null) { try { return JSON.stringify(t) } catch { return e } } getjson(t, e) { let s = e; const i = this.getdata(t); if (i) try { s = JSON.parse(this.getdata(t)) } catch { } return s } setjson(t, e) { try { return this.setdata(JSON.stringify(t), e) } catch { return !1 } } getScript(t) { return new Promise(e => { this.get({ url: t }, (t, s, i) => e(i)) }) } runScript(t, e) { return new Promise(s => { let i = this.getdata("@chavy_boxjs_userCfgs.httpapi"); i = i ? i.replace(/\n/g, "").trim() : i; let r = this.getdata("@chavy_boxjs_userCfgs.httpapi_timeout"); r = r ? 1 * r : 20, r = e && e.timeout ? e.timeout : r; const [o, h] = i.split("@"), a = { url: `http://${h}/v1/scripting/evaluate`, body: { script_text: t, mock_type: "cron", timeout: r }, headers: { "X-Key": o, Accept: "*/*" } }; this.post(a, (t, e, i) => s(i)) }).catch(t => this.logErr(t)) } loaddata() { if (!this.isNode()) return {}; { this.fs = this.fs ? this.fs : require("fs"), this.path = this.path ? this.path : require("path"); const t = this.path.resolve(this.dataFile), e = this.path.resolve(process.cwd(), this.dataFile), s = this.fs.existsSync(t), i = !s && this.fs.existsSync(e); if (!s && !i) return {}; { const i = s ? t : e; try { return JSON.parse(this.fs.readFileSync(i)) } catch (t) { return {} } } } } writedata() { if (this.isNode()) { this.fs = this.fs ? this.fs : require("fs"), this.path = this.path ? this.path : require("path"); const t = this.path.resolve(this.dataFile), e = this.path.resolve(process.cwd(), this.dataFile), s = this.fs.existsSync(t), i = !s && this.fs.existsSync(e), r = JSON.stringify(this.data); s ? this.fs.writeFileSync(t, r) : i ? this.fs.writeFileSync(e, r) : this.fs.writeFileSync(t, r) } } lodash_get(t, e, s) { const i = e.replace(/\[(\d+)\]/g, ".$1").split("."); let r = t; for (const t of i) if (r = Object(r)[t], void 0 === r) return s; return r } lodash_set(t, e, s) { return Object(t) !== t ? t : (Array.isArray(e) || (e = e.toString().match(/[^.[\]]+/g) || []), e.slice(0, -1).reduce((t, s, i) => Object(t[s]) === t[s] ? t[s] : t[s] = Math.abs(e[i + 1]) >> 0 == +e[i + 1] ? [] : {}, t)[e[e.length - 1]] = s, t) } getdata(t) { let e = this.getval(t); if (/^@/.test(t)) { const [, s, i] = /^@(.*?)\.(.*?)$/.exec(t), r = s ? this.getval(s) : ""; if (r) try { const t = JSON.parse(r); e = t ? this.lodash_get(t, i, "") : e } catch (t) { e = "" } } return e } setdata(t, e) { let s = !1; if (/^@/.test(e)) { const [, i, r] = /^@(.*?)\.(.*?)$/.exec(e), o = this.getval(i), h = i ? "null" === o ? null : o || "{}" : "{}"; try { const e = JSON.parse(h); this.lodash_set(e, r, t), s = this.setval(JSON.stringify(e), i) } catch (e) { const o = {}; this.lodash_set(o, r, t), s = this.setval(JSON.stringify(o), i) } } else s = this.setval(t, e); return s } getval(t) { return this.isSurge() || this.isLoon() ? $persistentStore.read(t) : this.isQuanX() ? $prefs.valueForKey(t) : this.isNode() ? (this.data = this.loaddata(), this.data[t]) : this.data && this.data[t] || null } setval(t, e) { return this.isSurge() || this.isLoon() ? $persistentStore.write(t, e) : this.isQuanX() ? $prefs.setValueForKey(t, e) : this.isNode() ? (this.data = this.loaddata(), this.data[e] = t, this.writedata(), !0) : this.data && this.data[e] || null } initGotEnv(t) { this.got = this.got ? this.got : require("got"), this.cktough = this.cktough ? this.cktough : require("tough-cookie"), this.ckjar = this.ckjar ? this.ckjar : new this.cktough.CookieJar, t && (t.headers = t.headers ? t.headers : {}, void 0 === t.headers.Cookie && void 0 === t.cookieJar && (t.cookieJar = this.ckjar)) } get(t, e = (() => { })) { t.headers && (delete t.headers["Content-Type"], delete t.headers["Content-Length"]), this.isSurge() || this.isLoon() ? (this.isSurge() && this.isNeedRewrite && (t.headers = t.headers || {}, Object.assign(t.headers, { "X-Surge-Skip-Scripting": !1 })), $httpClient.get(t, (t, s, i) => { !t && s && (s.body = i, s.statusCode = s.status), e(t, s, i) })) : this.isQuanX() ? (this.isNeedRewrite && (t.opts = t.opts || {}, Object.assign(t.opts, { hints: !1 })), $task.fetch(t).then(t => { const { statusCode: s, statusCode: i, headers: r, body: o } = t; e(null, { status: s, statusCode: i, headers: r, body: o }, o) }, t => e(t))) : this.isNode() && (this.initGotEnv(t), this.got(t).on("redirect", (t, e) => { try { if (t.headers["set-cookie"]) { const s = t.headers["set-cookie"].map(this.cktough.Cookie.parse).toString(); s && this.ckjar.setCookieSync(s, null), e.cookieJar = this.ckjar } } catch (t) { this.logErr(t) } }).then(t => { const { statusCode: s, statusCode: i, headers: r, body: o } = t; e(null, { status: s, statusCode: i, headers: r, body: o }, o) }, t => { const { message: s, response: i } = t; e(s, i, i && i.body) })) } post(t, e = (() => { })) { const s = t.method ? t.method.toLocaleLowerCase() : "post"; if (t.body && t.headers && !t.headers["Content-Type"] && (t.headers["Content-Type"] = "application/x-www-form-urlencoded"), t.headers && delete t.headers["Content-Length"], this.isSurge() || this.isLoon()) this.isSurge() && this.isNeedRewrite && (t.headers = t.headers || {}, Object.assign(t.headers, { "X-Surge-Skip-Scripting": !1 })), $httpClient[s](t, (t, s, i) => { !t && s && (s.body = i, s.statusCode = s.status), e(t, s, i) }); else if (this.isQuanX()) t.method = s, this.isNeedRewrite && (t.opts = t.opts || {}, Object.assign(t.opts, { hints: !1 })), $task.fetch(t).then(t => { const { statusCode: s, statusCode: i, headers: r, body: o } = t; e(null, { status: s, statusCode: i, headers: r, body: o }, o) }, t => e(t)); else if (this.isNode()) { this.initGotEnv(t); const { url: i, ...r } = t; this.got[s](i, r).then(t => { const { statusCode: s, statusCode: i, headers: r, body: o } = t; e(null, { status: s, statusCode: i, headers: r, body: o }, o) }, t => { const { message: s, response: i } = t; e(s, i, i && i.body) }) } } time(t, e = null) { const s = e ? new Date(e) : new Date; let i = { "M+": s.getMonth() + 1, "d+": s.getDate(), "H+": s.getHours(), "m+": s.getMinutes(), "s+": s.getSeconds(), "q+": Math.floor((s.getMonth() + 3) / 3), S: s.getMilliseconds() }; /(y+)/.test(t) && (t = t.replace(RegExp.$1, (s.getFullYear() + "").substr(4 - RegExp.$1.length))); for (let e in i) new RegExp("(" + e + ")").test(t) && (t = t.replace(RegExp.$1, 1 == RegExp.$1.length ? i[e] : ("00" + i[e]).substr(("" + i[e]).length))); return t } msg(e = t, s = "", i = "", r) { const o = t => { if (!t) return t; if ("string" == typeof t) return this.isLoon() ? t : this.isQuanX() ? { "open-url": t } : this.isSurge() ? { url: t } : void 0; if ("object" == typeof t) { if (this.isLoon()) { let e = t.openUrl || t.url || t["open-url"], s = t.mediaUrl || t["media-url"]; return { openUrl: e, mediaUrl: s } } if (this.isQuanX()) { let e = t["open-url"] || t.url || t.openUrl, s = t["media-url"] || t.mediaUrl; return { "open-url": e, "media-url": s } } if (this.isSurge()) { let e = t.url || t.openUrl || t["open-url"]; return { url: e } } } }; if (this.isMute || (this.isSurge() || this.isLoon() ? $notification.post(e, s, i, o(r)) : this.isQuanX() && $notify(e, s, i, o(r))), !this.isMuteLog) { let t = ["", "==============\ud83d\udce3\u7cfb\u7edf\u901a\u77e5\ud83d\udce3=============="]; t.push(e), s && t.push(s), i && t.push(i), console.log(t.join("\n")), this.logs = this.logs.concat(t) } } log(...t) { t.length > 0 && (this.logs = [...this.logs, ...t]), console.log(t.join(this.logSeparator)) } logErr(t, e) { const s = !this.isSurge() && !this.isQuanX() && !this.isLoon(); s ? this.log("", `\u2757\ufe0f${this.name}, \u9519\u8bef!`, t.stack) : this.log("", `\u2757\ufe0f${this.name}, \u9519\u8bef!`, t) } wait(t) { return new Promise(e => setTimeout(e, t)) } done(t = {}) { const e = (new Date).getTime(), s = (e - this.startTime) / 1e3; this.log("", `\ud83d\udd14${this.name}, \u7ed3\u675f! \ud83d\udd5b ${s} \u79d2`), this.log(), (this.isSurge() || this.isQuanX() || this.isLoon()) && $done(t) } }(t, e) }
+
